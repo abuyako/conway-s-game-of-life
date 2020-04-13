b9fbd29127b88b072655d11094a00479b6daee60
abuyako <abuyako@gmail.com>
2020-04-13 16:06:13 +0300

    adding prototype and print to the screen

--- life.js
+++ life.js
@@ -5,6 +5,15 @@
         this.height = seed.length;
         this.width = seed[0].length;
     };
+
+    _.prototype = {
+        next: function() {
+
+        },
+        toString: function() {
+
+        }
+    };
 })();
 
 var game = new Life([
@@ -14,3 +23,9 @@ var game = new Life([
     [0, 0, 1, 0, 0],
     [0, 0, 0, 0, 0]
 ]);
+
+console.log(game + '');
+
+game.next();
+
+console.log(game + '');
